4623a9fede7f3c98c07102cb2a05b676af6664e5
JBayona <jordavids_22@hotmail.com>
2021-11-26 16:03:22 -0600

    Add Auth Component.

--- react-amplified/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ react-amplified/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,5 +4,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "reactamplified4bfe86ef": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
